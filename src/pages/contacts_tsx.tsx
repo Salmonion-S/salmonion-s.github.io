552a8d44376387ac5b68f19e169c7a305734794c
Salmonions-S <karimons123ws@gmail.com>
2026-06-10 19:05:46 +0700

    Add GitHub Pages workflow

---
+++ src/pages/contacts_tsx.tsx
@@ -0,0 +1,9 @@
+import { NFCContact } from "../components/NFCContact";
+
+export default function Contacts() {
+  return (
+    <div className="flex-grow flex items-center justify-center py-20">
+      <NFCContact />
+    </div>
+  );
+}
